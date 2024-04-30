57e350f2a264c687a386a6017dfb10f3e51ff899
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-04-30 11:21:36 +0000

    datatypes conversion

---
+++ 01_Basics/Conversionconfusion.js
@@ -0,0 +1,18 @@
+let score = 33
+//console.log(typeof score); //method 1
+//console.log(typeof (score)); // method 2
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); 
+
+//"33" => 33
+// "33adc"=>NaN
+// "true" =>1 ;false => 0
+
+let isLoggedIn =1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//""=> false
+// "suhani" => true
