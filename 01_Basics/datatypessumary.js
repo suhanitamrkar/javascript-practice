6328be17532ab168427f772986b40bda31c098e0
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-04-30 15:28:38 +0000

    memory

--- 01_Basics/datatypessumary.js
+++ 01_Basics/datatypessumary.js
@@ -5,3 +5,12 @@
 // Reference (Non primtive)
 
 //Array , objects , function 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// stack (primitive ) ,Heap (Non-primitive)
+
+let myName = "suhani tamrkar"
+
+let anothername = myName
+anothername = "tanu tamrkar"
+console.log(anothername);
+console.log(myName);
