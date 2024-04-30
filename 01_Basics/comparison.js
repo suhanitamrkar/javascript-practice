e9bda57da1e48138fe70ac42e54b42959894e410
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-04-30 11:33:54 +0000

    datatypes comparison

---
+++ 01_Basics/comparison.js
@@ -0,0 +1,18 @@
+console.log( 2 > 4);
+console.log( 2 < 4);
+console.log( 2 == 4);
+console.log( 2 >= 4);
+console.log( 2 != 4);
+console.log( 2 > 4);
+
+console.log("2" >1);
+console.log("02" > 1);
+/*
+this type comparison should avoided
+console.log( null >0);
+console.log(null == 0);
+console.log( null >= 0);
+
+console.log(undefined == 0);
+console.log( undefined > 0);
+console.log( undefined <0 );*/
