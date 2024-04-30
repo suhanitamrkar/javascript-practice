d56f864745432b7ee9e28931839afd50302a1c83
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-04-30 11:03:04 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//alert("Hello my name is suhani") we are using node.js not browser
+let name = "suhani"
+let age = 19
+let isLoggedIn = false
+// number = 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+//symbol => unique
+
+
+// object 
+console.log(typeof null ); //object
+console.log(typeof undefined); //undefined
