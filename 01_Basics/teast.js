ddd25a9b35cd64e2e8e95c0e081dd00e8f9b830b
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-04-30 10:45:43 +0000

    create new file

---
+++ 01_Basics/teast.js
@@ -0,0 +1,16 @@
+console.log("suhani");
+const accountId = 1445325
+let accountEmail = "suhain@gmail.com"
+var accountPassword = "12345"
+accountCity = "Indore"
+let accountState;
+
+//accountId = 3 //not allowed
+/*
+prefer not to use var because of issue in block scope and  functional scope
+
+*/
+accountEmail = "hghghfgjdhfj"
+accountPassword = "43434343"
+accountCity = "jaipur"
+console.table([accountEmail,accountPassword,accountCity,accountState])
