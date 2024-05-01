7540119ba4a46c676ddf73bc4376959282ba3eb7
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-05-01 02:11:58 +0000

    number and math

---
+++ 01_Basics/number.js
@@ -0,0 +1,31 @@
+const score = 400 
+const balance = new Number(100)
+//console.log(balance)
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const othernumber = 123.4343
+
+//console.log(othernumber.toPrecision(3));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString("en-IN"));
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++
+console.log(Math);
+
+console.log(Math.abs(-5));
+console.log(Math.round(3.5));
+console.log(Math.random()*10);
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1);
+
+const min =10
+const max = 20
+
+Math.random() * (max -min +1)
+console.log(Math.floor(Math.random()* (max-min +1)) + min);
+
+
