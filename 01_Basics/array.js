401b502d8e1d8b9ec60f1427af721cb1a6634a2a
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-05-01 10:43:50 +0000

    advanced array

---
+++ 01_Basics/array.js
@@ -0,0 +1,42 @@
+//Array
+
+// const myArr = [1,23,34,232,23,23]
+
+// const myHeros = ["shaktimaan", "harrypoter"]
+// const myArr2 = new Array(2,3,4,5)
+// console.log(myArr[1]);
+
+//methods
+
+// myArr.push(4)
+// myArr.push(9)
+// myArr.pop()
+
+// myArr.unshift(4)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+
+
+// console.log(myArr);
+
+//slice, splice
+
+
+
+
+const another_array = [1,2,3,4,5,6,6,[8,8],9[8,0,[8,9,0]]]
+const real_aother_array = another_array.flat(Infinity)
+//console.log(real_aother_array);
+
+
+// console.log(Array.isArray("suhani"))
+// console.log(Array.from("suhani"));
+// console.log(Array.from({name:"suhNI"}));
+
+let score1 =100
+let score2 =300
+let score3 =300
+console.log(Array.of(score1,score2,score3));
+
+
