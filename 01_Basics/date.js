d65dbb690dade9d631b979c8d33eb5c00556b1a1
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-05-01 09:48:33 +0000

    learn data

---
+++ 01_Basics/date.js
@@ -0,0 +1,17 @@
+// dates
+
+let mydate = new Date()
+// console.log(mydate.toString());
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleString());
+//console.log(typeof mydate);
+
+let myCreatedDate = new Date(2023,0,23)
+//console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+
