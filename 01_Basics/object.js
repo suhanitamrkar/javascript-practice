54f34ceacbb81e02abbaba9a56eff50b099124f4
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-05-02 01:11:28 +0000

    object basic and advanced

--- 01_Basics/object.js
+++ 01_Basics/object.js
@@ -3,12 +3,35 @@
 // object literals
 //Object.create
 
-
+const mySym= Symbol("Key1")
 const jsuser = {
     name :"suhani",
     id:"3434",
+    [mySym]:"key3",
     age:19,
-    location :"indore"
+    email:"afjhdjfhdjf@gmai.con",
+    location :"indore",
+    isLoggedout:false
 
 
 }
+// console.log(jsuser.location);
+// console.log(jsuser["email"]);
+// console.log(jsuser["name"]);
+// console.log(jsuser.name);
+// console.log(jsuser[mySym]);
+
+jsuser.email = "shani@gmial.com"
+//console.log(jsuser);
+//Object.freeze(jsuser)
+jsuser.email = "suhanitamrkar@gmail.com"
+//console.log(jsuser);
+
+jsuser.greeting = function(){
+    console.log("Hello JS user");
+}
+console.log(jsuser.greeting());
+jsuser.greetingTwo= function(){
+    console.log(`Hello  jS user ${this.name}`);
+}
+console.log(jsuser.greetingTwo());
