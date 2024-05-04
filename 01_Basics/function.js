ad19405eb403a49d88bbd4a05048b19730c739c5
suhani tamrkar <149882700+suhanitamrkar@users.noreply.github.com>
2024-05-04 01:09:33 +0000

    advanced funtion

--- 01_Basics/function.js
+++ 01_Basics/function.js
@@ -20,4 +20,36 @@ function loginUserMessege (username){
 return `${username} just logged in `
 }
 
-console.log(loginUserMessege());
+//console.log(loginUserMessege());
+
+function calculateCartPrice(...num1){
+return num1
+}
+// function calculateCartPrice(val1,val2,...num1){ //output is 800,300 because val1 get 200 and val2 get 900
+//     return num1
+//     }
+//console.log(calculateCartPrice(200,900,800,300));
+
+// object in function
+const user = {
+    username :"suhani",
+    price:200
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+//handleObject(user)
+handleObject({
+    username:"hitesh",
+    price:"400"
+})
+
+// array in funtion
+
+const myNewArray = [200,434,44,234,4545]
+function returnSecondValue (getArray){
+    return getArray[2]
+}
+//console.log(returnSecondValue(myNewArray)); //output is 44 
+console.log(returnSecondValue([34,343,5454,232])); // output is 5454
